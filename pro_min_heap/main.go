510d64b53270ebb5f262ae7390299955d4941823
zevfang <fangwei0505@gmail.com>
2018-04-18 19:20:05 +0800

    update min heap

--- pro_min_heap/main.go
+++ pro_min_heap/main.go
@@ -1,56 +1,49 @@
 package main
 
 import (
-	"math/rand"
 	"fmt"
 	"container/heap"
 )
 
 /*
-*************继承container/heap 实现小顶堆****************
+*************继承container/heap 实现小顶堆的工作池****************
 golang标准库中提供了heap结构的容器，可以需要其中的几个方法，来实现一个堆类型的数据结构，
 使用时只需要调用标准库中提供的Init初始化接口、Pop接口、Push接口，就可以得到我们想要的结果。
 实现的方法有Len、Less、Swap、Push、Pop
 */
-type Request struct {
-	fn   func() int
-	data []byte
-	op   int
-	c    chan int
+
+type Item struct {
+	value    string //任意值&对象
+	priority int    // 队列中项的优先级
+	index    int    // 堆中项目的索引
 }
 
-type Worker struct {
-	req     chan Request
-	priority int
-	index   int
-	done    chan struct{}
-}
+// Pool实现 heap.Interface，并保存Worker
+type PriorityQueue []*Item
 
-type Pool []*Worker
-
-func (p Pool) Len() int {
+func (p PriorityQueue) Len() int {
 	return len(p)
 }
 
-func (p Pool) Less(i, j int) bool {
+func (p PriorityQueue) Less(i, j int) bool {
 	//最高优先所以使用大于。
 	return p[i].priority < p[j].priority
 }
 
-func (p Pool) Swap(i, j int) {
+func (p PriorityQueue) Swap(i, j int) {
 	p[i], p[j] = p[j], p[i]
 	p[i].index = i
 	p[j].index = j
 }
 
-func (p *Pool) Push(x interface{}) {
+func (p *PriorityQueue) Push(x interface{}) {
 	n := len(*p)
-	item := x.(*Worker)
+	item := x.(*Item)
 	item.index = n
 	*p = append(*p, item)
 }
 
-func (p *Pool) Pop() interface{} {
+func (p *PriorityQueue) Pop() interface{} {
 	old := *p
 	n := len(*p)
 	item := old[n-1]
@@ -58,51 +51,29 @@ func (p *Pool) Pop() interface{} {
 	return item
 }
 
-var (
-	MAXWORKS = 10000
-	MAXQUEUE = 1000
-)
-
 func main() {
-	pool := new(Pool)
-	for i := 0; i < 9; i++ {
-		work := &Worker{
-			req:     make(chan Request, MAXQUEUE),
-			priority: rand.Intn(100),
-			index:   i,
-		}
-		fmt.Println("pengding:", work.priority, "--- i:", i)
-		heap.Push(pool, work)
+	items := map[string]int{
+		"zhangsan": 6, "lisi": 4, "wangwu": 50,
 	}
 
-	heap.Init(pool)
-	fmt.Println("初始化堆成功", pool)
-
-	//for pool.Len() > 0 {
-	//	worker := heap.Pop(pool).(*Worker)
-	//	fmt.Println("pengding:", worker.pending, "--- i:", worker.index)
-	//}
-
-	worker1 := heap.Pop(pool).(*Worker)
-
-	fmt.Println("pengding:", worker1.priority, "--- i:", worker1.index)
-
-}
-
-func (w *Worker) Run() {
-	go func() {
-		for {
-			select {
-			case req := <-w.req:
-				req.c <- req.fn()
-			case <-w.done:
-				break
-			}
+	fmt.Println("初始化队列池")
+	pool := make(PriorityQueue, len(items))
+	i := 0
+	for value, priority := range items {
+		pool[i] = &Item{
+			value:    value,
+			priority: priority,
+			index:    i,
 		}
-	}()
-}
+		fmt.Println("priority:", pool[i].priority, "--- i:", pool[i].value)
+		i++
+	}
+	heap.Init(&pool)
 
-func (w *Worker) Stop()  {
-	w.done<- struct{}{}
-}
+	fmt.Println("开始按优先级获取值：")
+	for pool.Len() > 0 {
+		item := heap.Pop(&pool).(*Item)
+		fmt.Println("priority:", item.priority, "--- i:", item.value)
+	}
 
+}
